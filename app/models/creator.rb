62b8cc5e7b2f30e925328b5f27f503c0686464b6
Matt Eva <matteweva@gmail.com>
2022-01-11 16:39:34 -0800

    added creator relationships

--- app/models/creator.rb
+++ app/models/creator.rb
@@ -1,3 +1,22 @@
 class Creator < ApplicationRecord
   belongs_to :user
+  
+  has_many :creator_taglinks
+  has_many :tags, through: :creator_taglinks
+  
+  has_many :writings
+  has_many :writ_donations
+  has_many :donated_writs, through: :writ_donations, source: :writing
+
+  has_many :audios
+  has_many :aud_donations
+  has_many :donated_auds, through: :aud_donations, source: :audio
+
+  has_many :arts
+  has_many :art_donations
+  has_many :donated_arts, through: :art_donations, source: :art
+
+  has_many :videos
+  has_many :vid_donations
+  has_many :donated_vids, through: :vid_donations, source: :video
 end
