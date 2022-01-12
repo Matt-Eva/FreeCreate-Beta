030d577a02d84b4b1558b6b682c9cfe7273f10fd
Matt Eva <matteweva@gmail.com>
2022-01-12 15:38:00 -0800

    added logout functionality

--- client/src/components/navigation/ProfileDropdown.js
+++ client/src/components/navigation/ProfileDropdown.js
@@ -1,6 +1,19 @@
 import Dropdown from "react-bootstrap/Dropdown"
+import {useDispatch} from "react-redux"
+import {removeUser} from "../userauth/userSlice"
+import {useNavigate} from "react-router-dom"
 
 function ProfileDropdown() {
+const dispatch = useDispatch()
+const navigate = useNavigate()
+
+    function logout(){
+        fetch("/logout", {method: "DELETE"})
+        .then(() =>{
+            dispatch(removeUser())
+            navigate("/loggedout")
+        })
+    }
     return (
         <Dropdown>
             <Dropdown.Toggle variant="success" id="dropdown-basic">
@@ -9,7 +22,7 @@ function ProfileDropdown() {
             <Dropdown.Menu>
                 <Dropdown.Item>View Profile</Dropdown.Item>
                 <Dropdown.Item>Creator Profiles</Dropdown.Item>
-                <Dropdown.Item>Logout</Dropdown.Item>
+                <Dropdown.Item onClick={logout}>Logout</Dropdown.Item>
             </Dropdown.Menu>
         </Dropdown>
     )
