1eba7b5ed9ead271607774da29147c608d558342
Matt Eva <matteweva@gmail.com>
2022-01-11 17:47:33 -0800

    tag relations established

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,16 @@
 class Tag < ApplicationRecord
+    has_many :writ_taglinks
+    has_many :writings, through: :writ_taglinks
+
+    has_many :aud_taglinks
+    has_many :audios, through: :aud_taglinks
+
+    has_many :creator_taglinks
+    has_many :creators, through: :creator_taglinks
+
+    has_many :art_taglinks
+    has_many :arts, through: :art_taglinks
+
+    has_many :vid_taglinks
+    has_many :videos, through: :vid_taglinks
 end
