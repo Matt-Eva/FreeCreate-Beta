6106590aa88248c414b7b19f0a5e998181db22a3
Matt Eva <matteweva@gmail.com>
2022-01-13 15:27:01 -0800

    writing form partial buildout

--- client/src/components/forms/CreateCreationPage.js
+++ client/src/components/forms/CreateCreationPage.js
@@ -1,5 +1,7 @@
 import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Image from "react-bootstrap/Image"
 import TopNav from "../navigation/TopNav"
 import { useState } from "react"
 import CreateArtForm from "./CreateArtForm"
@@ -9,6 +11,7 @@ import CreateVideoForm from "./CreateVideoForm"
 
 function CreateCreationPage() {
     const [contentType, setContentType] = useState("")
+    const [thumbnail, setThumbnail] = useState(null)
     console.log(contentType)
     return (
         <Container>
@@ -16,14 +19,19 @@ function CreateCreationPage() {
                 <TopNav />
             </Row>
             <Row>
-                <h1>Create!</h1>
-                <select onChange={(e) => setContentType(e.target.value)}>
-                    <option value="">Select Creation Type</option>
-                    <option value="writing">Writing</option>
-                    <option value="audio">Audio</option>
-                    <option value="art">Art(Images)</option>
-                    <option value="video">Video</option>
-                </select>
+                <Col>
+                    <h1>Create!</h1>
+                    <select onChange={(e) => setContentType(e.target.value)}>
+                        <option value="">Select Creation Type</option>
+                        <option value="writing">Writing</option>
+                        <option value="audio">Audio</option>
+                        <option value="art">Art(Images)</option>
+                        <option value="video">Video</option>
+                    </select>
+                </Col>
+                <Col>
+                    {thumbnail ? <Image src={thumbnail}/> : <h2><em>Your Thumbnail Here</em></h2>}
+                </Col>
             </Row>
             <Row>
                 {contentType === "writing" ? <CreateWritingForm/>: null}
--- client/src/components/forms/CreateWritingForm.js
+++ client/src/components/forms/CreateWritingForm.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import Form from 'react-bootstrap/Form'
+import useState from 'react'
 
 function CreateWritingForm() {
+    const [uploadData, setUploadData] = useState({
+        title: "",
+        thumbnail: "",
+        content: "",
+        category: ""
+    })
     return (
-        <div>
-            
-        </div>
+        <>
+        <h4>Add Writing:</h4>
+        <Form>
+            <Form.Group>
+                <Form.Label>Title:</Form.Label>
+                <Form.Control type="text"/>
+            </Form.Group>
+        </Form>
+        </>
     )
 }
 
