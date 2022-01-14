43b1d1409a9a15ed76ce813c2c4bd1428610ba25
Matt Eva <matteweva@gmail.com>
2022-01-14 11:09:30 -0800

    added clear file functionality for incorrect file types

--- client/src/components/forms/CreateCreatorProfile.js
+++ client/src/components/forms/CreateCreatorProfile.js
@@ -5,15 +5,23 @@ import Form from "react-bootstrap/Form"
 import Button from "react-bootstrap/Button"
 import Image from "react-bootstrap/Image"
 import {useSelector} from "react-redux"
-import { useState } from "react"
+import { useState, useRef } from "react"
 import TopNav from "../navigation/TopNav"
 
 function CreateCreatorProfile() {
     const user = useSelector(state => state.user.user)
+    const ref = useRef()
     const [creatorName, setCreatorName] = useState("")
+    const [creatorType, setCreatorType] = useState({
+            is_writer: false,
+            is_audio: false,
+            is_artist: false,
+            is_video: false,
+    })
     const [creatorThumbnail, setCreatorThumbnail] = useState(null)
     const [thumbnailDisplay, setThumbnailDisplay] = useState(null)
     const [loading, setLoading] = useState(false)
+    console.log(creatorType)
 
     function picChangeHandler(e){
         const thumbnail = e.target.files[0]
@@ -23,6 +31,8 @@ function CreateCreatorProfile() {
             setCreatorThumbnail(thumbnail)
         } else{
             alert("That is not an appropriate image file.")
+            ref.current.value=""
+            console.log(e.target.value)
         } 
     }
 
@@ -51,6 +61,7 @@ function CreateCreatorProfile() {
                 } else{
                     setLoading(false)
                     alert("You did not select a valid image file")
+                    ref.current.value=""
                 }
             })     
         } else {
@@ -70,10 +81,10 @@ function CreateCreatorProfile() {
             user_id: user.id,
             name: creatorName,
             prof_pic: thumbnailDisplay,
-            is_writer: false,
-            is_audio: false,
-            is_artist: false,
-            is_video: false,
+            is_writer: creatorType.is_writer,
+            is_audio: creatorType.is_audio,
+            is_artist: creatorType.is_artist,
+            is_video: creatorType.is_video,
             payment_info: null
         }
     }
@@ -85,7 +96,7 @@ function CreateCreatorProfile() {
             </Row>
             <Row>
                 <Col>
-                    <h1>Create Creator</h1>
+                    <h1>Create A New Creator Profile</h1>
                 </Col>
                 <Col>
                     {loading ? <p>Loading Profile pic...</p> : null}
@@ -97,12 +108,12 @@ function CreateCreatorProfile() {
                 {thumbnailDisplay === null ? <Form onSubmit={submitPic} >
                     <Form.Group>
                         <Form.Label>Upload your Creator profile picture <em>(Optional)</em></Form.Label>
-                        <Form.Control type="file" name="file" onChange={picChangeHandler}/>
+                        <Form.Control type="file" name="file" ref={ref} onChange={picChangeHandler}/>
                         <Button type="submit">Add Prof Pic</Button>
                     </Form.Group>
                 </Form> : null}
             </Row>
-            <Form onChange={nameChange} onSubmit={submitCreator}>
+            <Form onChange={nameChange}>
                 <Form.Group>
                     <Form.Label>Creator Name:</Form.Label>
                     <Form.Control type="text" name="name" value={creatorName}/>
@@ -110,17 +121,17 @@ function CreateCreatorProfile() {
             </Form>
             <Form>
                 <Form.Group>
-                    <Form.Label>How would you describe yourself? (Select all that apply)</Form.Label>
+                    <Form.Label>How would you describe yourself? Select all that apply.</Form.Label>
                 </Form.Group>
                 <Form.Group>
                     <Form.Label>Writer</Form.Label>
-                    <Form.Check type="switch" checked={false}/>
+                    <Form.Check type="switch" checked={creatorType.is_writer} onChange={() => setCreatorType({...creatorType, is_writer: !creatorType.is_writer})}/>
                     <Form.Label>Musician / Audio Producer</Form.Label>
-                    <Form.Check type="switch"/>
+                    <Form.Check type="switch" checked={creatorType.is_audio} onChange={() => setCreatorType({...creatorType, is_audio: !creatorType.is_audio})}/>
                     <Form.Label>Visual Artist / Photographer</Form.Label>
-                    <Form.Check type="switch"/>
+                    <Form.Check type="switch" checked={creatorType.is_artist} onChange={() => setCreatorType({...creatorType, is_artist: !creatorType.is_artist})}/>
                     <Form.Label>Video / Film Maker </Form.Label>
-                    <Form.Check type="switch"/>
+                    <Form.Check type="switch" checked={creatorType.is_video} onChange={() => setCreatorType({...creatorType, is_video: !creatorType.is_video})}/>
                 </Form.Group>
             </Form>
         </Container>
