9867aa88f8a98273efb965983b5694d94e8ac3ad
Matt Eva <matteweva@gmail.com>
2022-01-14 13:56:32 -0800

    creator selection implemented

--- client/src/components/forms/CreateCreationPage.js
+++ client/src/components/forms/CreateCreationPage.js
@@ -15,14 +15,24 @@ import TopNav from "../navigation/TopNav"
 function CreateCreationPage() {
     const user = useSelector(state => state.user.user)
     const [contentType, setContentType] = useState("")
-    const [creator, setCreator] = useState(null)
+    const [creatorId, setCreatorId] = useState(null)
     const [thumbnail, setThumbnail] = useState(null)
-    console.log(contentType)
+    // console.log(contentType)
+    // console.log(creatorId)
 
-    let creatorOptions = [] 
+    let creatorOptions = []
+    let displayCreator = undefined; 
     if (user !== null){
-        creatorOptions = user.creators?.map(creator => <option value={creator.name}>{creator.name}</option>)
+        creatorOptions = user.creators?.map(option => <option key={option.id} value={option.id}>{option.name}</option>)
+        // console.log(user.creators[0].id)
+        // user.creators.forEach(option => console.log(option.id))
+        if (creatorId !== null && user.creators.length !== 0){
+            console.log(creatorId)
+            console.log(user.creators[0].id)
+            displayCreator = user.creators.filter(option => option.id === creatorId)[0]
+        }
     }
+    console.log(displayCreator)
     
     if (creatorOptions.length === 0){
         return(
@@ -47,19 +57,23 @@ function CreateCreationPage() {
                 <TopNav />
             </Row>
             <Row>
-            <h1>Create!</h1>
-            {creatorOptions.length === 0 ?  <div>
-                <h2>You haven't set up any creator profiles</h2>
-                <Link to="/newcreator"><Button>Create a Creator Profile</Button></Link>
-                </div> :
-                    <select>
-                        <option>Select Creator Profile</option>
+                <Col>
+                    <h1>Create!</h1>
+                    <select onChange={(e) => setCreatorId(parseInt(e.target.value))}>
+                        <option value={"0"} >Select Creator Profile</option>
                         {creatorOptions}
-                    </select>}
+                    </select>
+                </Col>
+                <Col>
+                {displayCreator !== undefined ? <div>
+                    <Image src={displayCreator.prof_pic} style={{"height": "100px"}}/>
+                    <h4>{displayCreator.name}</h4>
+                </div> : null}
+                </Col>
             </Row>
             <Row>
                 <Col>
-                    {creator ? <h2>{creator.name}</h2> : <h2>Please choose a creator Profile</h2>}
+                    {/* {creator ? <h2>{creator.name}</h2> : <h2>Please choose a creator Profile</h2>} */}
                     <select onChange={(e) => setContentType(e.target.value)}>
                         <option value="">Select Creation Type</option>
                         <option value="writing">Writing</option>
