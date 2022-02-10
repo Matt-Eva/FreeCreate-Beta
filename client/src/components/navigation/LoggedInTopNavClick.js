9a75d8ccfd10f932eac4b9eec47cee08698b0a4a
Matt Eva <matteweva@gmail.com>
2022-02-10 14:45:29 -0800

    changed topnav styles

--- client/src/components/navigation/LoggedInTopNavClick.js
+++ client/src/components/navigation/LoggedInTopNavClick.js
@@ -30,13 +30,13 @@ function LoggedInTopNavClick() {
         <Container fluid>
             <Row className="text-center">
                 <Col className="d-flex justify-content-center">
-                    {displayType === "writing" ? <NavLink to="/"onClick={setWriting} className="me-3 pe-2 ps-2 rounded" style={{"background": "hsl(180, 90%, 90%)"}}><h3>Writing</h3></NavLink> :<NavLink to="/" className="me-3 pe-2 ps-2 rounded" onClick={setWriting}><h3>Writing</h3></NavLink>}
+                    {displayType === "writing" ? <NavLink to="/"onClick={setWriting} className="me-3 pe-2 ps-2 rounded" style={{"background": "hsl(180, 0%, 90%)"}}><h3>Writing</h3></NavLink> :<NavLink to="/" className="me-3 pe-2 ps-2 rounded" onClick={setWriting}><h3>Writing</h3></NavLink>}
 
-                    {displayType === "audio" ? <NavLink to="/" onClick={setAudio} className="me-3 pe-2 ps-2 rounded" style={{"background": "hsl(180, 90%, 90%)"}}><h3>Audio</h3></NavLink> :<NavLink to="/" className="me-3 pe-2 ps-2 rounded" onClick={setAudio}><h3>Audio</h3></NavLink> }
+                    {displayType === "audio" ? <NavLink to="/" onClick={setAudio} className="me-3 pe-2 ps-2 rounded" style={{"background": "hsl(180, 0%, 90%)"}}><h3>Audio</h3></NavLink> :<NavLink to="/" className="me-3 pe-2 ps-2 rounded" onClick={setAudio}><h3>Audio</h3></NavLink> }
 
-                    {displayType === "art" ? <NavLink to="/" className="me-3 pe-2 ps-2 rounded" onClick={setArt} style={{"background": "hsl(180, 90%, 90%)"}}><h3>Art</h3></NavLink> :<NavLink to="/" className="me-3 pe-2 ps-2 rounded"><h3 onClick={setArt}>Art</h3></NavLink> }
+                    {displayType === "art" ? <NavLink to="/" className="me-3 pe-2 ps-2 rounded" onClick={setArt} style={{"background": "hsl(180, 0%, 90%)"}}><h3>Art</h3></NavLink> :<NavLink to="/" className="me-3 pe-2 ps-2 rounded"><h3 onClick={setArt}>Art</h3></NavLink> }
 
-                    {displayType === "video" ? <NavLink to="/" onClick={setVideo} className="pe-2 ps-2 rounded" style={{"background": "hsl(180, 90%, 90%)"}}><h3>Video</h3></NavLink> :<NavLink to="/" className="pe-2 ps-2 rounded" onClick={setVideo}><h3 >Video</h3></NavLink> }
+                    {displayType === "video" ? <NavLink to="/" onClick={setVideo} className="pe-2 ps-2 rounded" style={{"background": "hsl(180, 0%, 90%)"}}><h3>Video</h3></NavLink> :<NavLink to="/" className="pe-2 ps-2 rounded" onClick={setVideo}><h3 >Video</h3></NavLink> }
                 </Col>
             </Row>
         </Container>
